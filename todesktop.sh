305b37a72fa6d6fbfa8f01f8b021b60fa625a8c3
hongliang <hongliang5316@gmail.com>
2017-06-22 04:10:36 -0400

    delete useless files

--- todesktop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-scp -r /root/rpmbuild/RPMS/noarch/$1 hongliang@10.211.55.2:/Users/hongliang/Desktop
--- tojs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-scp -r /root/rpmbuild/RPMS/noarch/$1 hongliang@js.yfcdn.net:~/
